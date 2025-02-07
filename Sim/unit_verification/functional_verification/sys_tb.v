abb53769123b06d87f6bc0a6aa91b61689e55141
Niv102002 <71827909+Niv102002@users.noreply.github.com>
2025-02-07 21:10:07 +0530

    systolic_array 2x2 tb working

--- Sim/unit_verification/functional_verification/sys_tb.v
+++ Sim/unit_verification/functional_verification/sys_tb.v
@@ -6,23 +6,23 @@ module systolic_2x2_tb;
     reg clk;
     reg rst;
     reg load_in;
-    reg [31:0] mat1_row0, mat1_row1;
-    reg [31:0] mat2_col0, mat2_col1;
+    reg [31:0] row_in_row0, row_in_row1;
+    reg [31:0] col_in_col0, col_in_col1;
 
     // Outputs
     wire [63:0] result_row00, result_row01, result_row10, result_row11;
     wire carry_00, carry_01, carry_10, carry_11;
-    // wire done;
+     wire done;
 
     // Instantiate the Unit Under Test (UUT)
     systolic_2x2 #(.DATA_TYPE(3'b011)) uut (
         .clk(clk),
         .rst(rst),
         .load_in(load_in),
-        .mat1_row0(mat1_row0),
-        .mat1_row1(mat1_row1),
-        .mat2_col0(mat2_col0),
-        .mat2_col1(mat2_col1),
+        .row_in_row0(row_in_row0),
+        .row_in_row1(row_in_row1),
+        .col_in_col0(col_in_col0),
+        .col_in_col1(col_in_col1),
         .result_row00(result_row00),
         .result_row01(result_row01),
         .result_row10(result_row10),
@@ -30,8 +30,8 @@ module systolic_2x2_tb;
         .carry_00(carry_00),
         .carry_01(carry_01),
         .carry_10(carry_10),
-        .carry_11(carry_11)
-        // .done(done)
+        .carry_11(carry_11),
+        .done(done)
     );
 
     // Clock generation
@@ -45,65 +45,65 @@ module systolic_2x2_tb;
         // Initialize inputs
         rst = 1;
         load_in = 0;
-        mat1_row0 = 0;
-        mat1_row1 = 0;
-        mat2_col0 = 0;
-        mat2_col1 = 0;
+        row_in_row0 = 0;
+        row_in_row1 = 0;
+        col_in_col0 = 0;
+        col_in_col1 = 0;
 
         // Apply reset
         #20;
         rst = 0;
 
         // Load first set of data
-        #200;
+        #100;
         load_in = 1;
-        mat1_row0 = 32'h4; 
-        mat1_row1 = 32'h0; 
+        row_in_row0 = 32'd2; //
+        row_in_row1 = 32'h0; 
 
-        mat2_col0 = 32'h8; 
-        mat2_col1 = 32'h0; 
+        col_in_col0 = 32'h2; 
+        col_in_col1 = 32'h0; 
  
-        #20 load_in = 0;
+        #10 load_in = 0;
 
         // Wait for done signal
         // wait(done == 1);
-        #300;
+        #200;
 
         // 2nd set of data
         load_in = 1;
-        mat1_row0 = 32'h2; 
-        mat1_row1 = 32'h1; 
+        row_in_row0 = 32'h2; 
+        row_in_row1 = 32'h2; 
 
-        mat2_col0 = 32'h2; 
-        mat2_col1 = 32'h1; 
+        col_in_col0 = 32'h2; 
+        col_in_col1 = 32'h2; 
 
-        #20 load_in = 0;
+        #10 load_in = 0;
 
         // // Wait for done signal
         // wait(done == 1);
 
-        #300;
+        #200;
         load_in = 1;
-        mat1_row0 = 32'h0; 
-        mat1_row1 = 32'h8; 
+        row_in_row0 = 32'h0; 
+        row_in_row1 = 32'h2; 
 
-        mat2_col0 = 32'h0; 
-        mat2_col1 = 32'h4; 
+        col_in_col0 = 32'h0; 
+        col_in_col1 = 32'h2; 
 
-        #20 load_in = 0;
+        #10 load_in = 0;
 
         //Wait for done signal
         // wait(done == 1);
 
-        #300;
+        #200;
         load_in = 1;
-        mat1_row0 = 32'h0; 
-        mat1_row1 = 32'h0; 
+        row_in_row0 = 32'h0; 
+        row_in_row1 = 32'h0; 
 
-        mat2_col0 = 32'h0; 
-        mat2_col1 = 32'h0; 
+        col_in_col0 = 32'h0; 
+        col_in_col1 = 32'h0; 
 
-        #20 load_in = 0;
+        #10 load_in = 0;
 
         // Wait for done signal
         // wait(done == 1);
@@ -117,5 +117,10 @@ module systolic_2x2_tb;
         $finish;
     end
 
+    // Monitor outputs
+    // initial begin
+    //     $monitor("Time: %0t | Done: %b | Result Row00: %h | Result Row01: %h | Result Row10: %h | Result Row11: %h",
+    //              $time, done, result_row00, result_row01, result_row10, result_row11);
+    // end
 
 endmodule
