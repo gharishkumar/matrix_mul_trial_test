fa8bce11b4b3745b322ac81bb9d9a8ab52a0f317
Niv102002 <71827909+Niv102002@users.noreply.github.com>
2025-02-07 21:09:02 +0530

    int 32 matrix multiplier 2x2 working

--- Design/matrix_multiplier/PE/pe.v
+++ Design/matrix_multiplier/PE/pe.v
@@ -13,49 +13,57 @@ module pe #(parameter DATA_TYPE = 3'b011 )
     input             clk,
     input             rst,
     input             load_in,
-    input [31:0]      mat1, // parameter for datawidth to be added accordingly
-    input [31:0]      mat2, 
-    output reg [31:0] outp_col, 
-    output reg [31:0] outp_row, 
-    output reg [64:0] pe_result
-    // output reg        done_pe
+    input [31:0]      row_in, // parameter for datawidth to be added accordingly
+    input [31:0]      col_in, 
+    output reg [31:0] col_out, 
+    output reg [31:0] row_out, 
+    output reg [64:0] pe_result,
+    output reg        done_pe
 );
     
+    vedic32x32 inst_vedic32x32 (.clk(clk), .reset(rst), .a(a), .b(b), .do(do_mul), .result(result), .done(done));
 
-wire [31:0] a;
-wire [31:0] b;
-wire [63:0] result;
+    wire [31:0] a;
+    wire [31:0] b;
+    wire [63:0] result;
 
-reg [31:0] a_in_reg;
-reg [31:0] b_in_reg;
+    wire do_mul;
+    reg do_mul_reg;
 
-assign a = a_in_reg;
-assign b = b_in_reg;
+    assign do_mul = do_mul_reg;
 
-vedic32x32 inst_vedic32x32 (.a(a), .b(b), .clk(clk), .reset(rst), .result(result), .valid_out(valid_out));
+    reg [31:0] a_in_reg;
+    reg [31:0] b_in_reg;
+
+    assign a = a_in_reg;
+    assign b = b_in_reg;
 
     always @(posedge clk) begin 
         if(rst) begin
             a_in_reg <= 0;
             b_in_reg <= 0;
             pe_result <= 0;
-            outp_col <= 0;
-            outp_row <= 0;
-            // done_pe   <= 1'b0;
+            col_out <= 0;
+            row_out <= 0;
+            do_mul_reg <= 1'b0;
+            done_pe  <= 1'b0;
         end else begin
             if (load_in) begin
-                a_in_reg <= mat1;
-                b_in_reg <= mat2;
-                // done_pe   <= 1'b0;
+                a_in_reg   <= row_in;
+                b_in_reg   <= col_in;
+                do_mul_reg <= 1'b1;
+                done_pe    <= 1'b0;
             end else begin
-                if(valid_out) begin
+                if(done) begin
                     pe_result <= pe_result + result;
-                    // done_pe   <= 1'b1;
-                    outp_row  <= a_in_reg;
-                    outp_col  <= b_in_reg;
+                    done_pe   <= 1'b1;
+                    do_mul_reg <= 1'b0;
+                    row_out  <= a_in_reg;
+                    col_out  <= b_in_reg;
                 end else begin
-                    pe_result <= pe_result;
-                    // done_pe   <= 1'b0;
+                    pe_result  <= pe_result;
+                    done_pe    <= 1'b0;
+                    do_mul_reg <= 1'b0;
                 end
             end
         end
--- Design/matrix_multiplier/PE/sys_arry.v
+++ Design/matrix_multiplier/PE/sys_arry.v
@@ -4,18 +4,18 @@ module systolic_2x2 #(parameter DATA_TYPE = 3'b011) (
     input clk,
     input rst,
     input load_in,
-    input [31:0] mat1_row0, mat1_row1, // Matrix A inputs (2 rows)
-    input [31:0] mat2_col0, mat2_col1, // Matrix B inputs (2 columns)
+    input [31:0] row_in_row0, row_in_row1, // Matrix A inputs (2 rows)
+    input [31:0] col_in_col0, col_in_col1, // Matrix B inputs (2 columns)
     output [63:0] result_row00, result_row01, result_row10, result_row11,// Output matrix C (2 rows)
-    output carry_00, carry_01, carry_10, carry_11
-    // output done
+    output carry_00, carry_01, carry_10, carry_11,
+    output reg done
 );
 
     // Internal wires for PE communication
-    wire [31:0] pe00_outp_row, pe00_outp_col;
-    wire [31:0] pe01_outp_row, pe01_outp_col;
-    wire [31:0] pe10_outp_row, pe10_outp_col;
-    wire [31:0] pe11_outp_row, pe11_outp_col;
+    wire [31:0] pe00_row_out, pe00_col_out;
+    wire [31:0] pe01_row_out, pe01_col_out;
+    wire [31:0] pe10_row_out, pe10_col_out;
+    wire [31:0] pe11_row_out, pe11_col_out;
 
     wire [64:0] pe00_result, pe01_result, pe10_result, pe11_result;
 
@@ -26,12 +26,12 @@ module systolic_2x2 #(parameter DATA_TYPE = 3'b011) (
         .clk(clk),
         .rst(rst),
         .load_in(load_in),
-        .mat1(mat1_row0),
-        .mat2(mat2_col0),
-        .outp_row(pe00_outp_row),
-        .outp_col(pe00_outp_col),
-        .pe_result(pe00_result)
-        // .done_pe(pe00_done)
+        .row_in(row_in_row0),
+        .col_in(col_in_col0),
+        .row_out(pe00_row_out),
+        .col_out(pe00_col_out),
+        .pe_result(pe00_result),
+        .done_pe(pe00_done)
     );
 
     // Instantiate PE for (0,1)
@@ -39,12 +39,12 @@ module systolic_2x2 #(parameter DATA_TYPE = 3'b011) (
         .clk(clk),
         .rst(rst),
         .load_in(load_in),
-        .mat1(pe00_outp_row), // Pass row output from PE00
-        .mat2(mat2_col1),
-        .outp_row(pe01_outp_row),
-        .outp_col(pe01_outp_col),
-        .pe_result(pe01_result)
-        // .done_pe(pe01_done)
+        .row_in(pe00_row_out), // Pass row output from PE00
+        .col_in(col_in_col1),
+        .row_out(pe01_row_out),
+        .col_out(pe01_col_out),
+        .pe_result(pe01_result),
+        .done_pe(pe01_done)
     );
 
     // Instantiate PE for (1,0)
@@ -52,12 +52,12 @@ module systolic_2x2 #(parameter DATA_TYPE = 3'b011) (
         .clk(clk),
         .rst(rst),
         .load_in(load_in),
-        .mat1(mat1_row1),
-        .mat2(pe00_outp_col), // Pass column output from PE00
-        .outp_row(pe10_outp_row),
-        .outp_col(pe10_outp_col),
-        .pe_result(pe10_result)
-        // .done_pe(pe10_done)
+        .row_in(row_in_row1),
+        .col_in(pe00_col_out), // Pass column output from PE00
+        .row_out(pe10_row_out),
+        .col_out(pe10_col_out),
+        .pe_result(pe10_result),
+        .done_pe(pe10_done)
     );
 
     // Instantiate PE for (1,1)
@@ -65,12 +65,12 @@ module systolic_2x2 #(parameter DATA_TYPE = 3'b011) (
         .clk(clk),
         .rst(rst),
         .load_in(load_in),
-        .mat1(pe10_outp_row), // Pass row output from PE10
-        .mat2(pe01_outp_col), // Pass column output from PE01
-        .outp_row(pe11_outp_row),
-        .outp_col(pe11_outp_col),
-        .pe_result(pe11_result)
-        // .done_pe(pe11_done)
+        .row_in(pe10_row_out), // Pass row output from PE10
+        .col_in(pe01_col_out), // Pass column output from PE01
+        .row_out(pe11_row_out),
+        .col_out(pe11_col_out),
+        .pe_result(pe11_result),
+        .done_pe(pe11_done)
     );
 
     // Assign final results
@@ -79,6 +79,27 @@ module systolic_2x2 #(parameter DATA_TYPE = 3'b011) (
     assign {carry_01, result_row01} = pe01_result;
     assign {carry_10, result_row10} = pe10_result;
     assign {carry_11, result_row11} = pe11_result;
+
+    reg [2:0] count;
+
+    always @(posedge clk) begin 
+        if(rst) begin
+            count <= 0;
+            done  <= 0;
+        end else begin
+            if (pe00_done || pe01_done || pe10_done || pe11_done) begin
+                count <= count + 1;
+                done  <= 0;
+            end else if (count == 3'b100) begin
+                done  <= 1'b1;
+                count <= 3'b000;
+            end else begin
+                done  <= 0;
+                count <= count;
+            end
+        end
+    end
+
    
 
 endmodule
